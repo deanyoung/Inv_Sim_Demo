108519ca5ce05c797e2d757d2943e366040c9d11
Dean <youngde@reed.edu>
2015-09-04 14:07:33 -0700

    back to default values

--- server.R
+++ server.R
@@ -72,17 +72,17 @@ shinyServer(function(input, output) {
       s <- s.returns[i]
       b <- b.returns[i]
 
-      s <- s + .1
-      b <- b + .1
+      #s <- s + .1
+      #b <- b + .1
       port.split.new <- port.split * exp(c(s,b))
       port.change <<- port.split * c(exp(s)-1,exp(b)-1)
       port.value <<- port.split.new[1] + port.split.new[2]
       }
       
-     #s.avg <- sum(s.returns[(count+1):(count+period)])
-     #b.avg <- sum(b.returns[(count+1):(count+period)])
-     s.avg <- sum(s.returns[(count+1):(count+period)]+.1)
-     b.avg <- sum(b.returns[(count+1):(count+period)]+.1)
+     s.avg <- sum(s.returns[(count+1):(count+period)])
+     b.avg <- sum(b.returns[(count+1):(count+period)])
+     #s.avg <- sum(s.returns[(count+1):(count+period)]+.1)
+     #b.avg <- sum(b.returns[(count+1):(count+period)]+.1)
       performance.data <<- as.data.frame(matrix(c("A","B",(exp(s.avg/period)-1)*100, (exp(b.avg/period)-1)*100),2)) %>%
                                         mutate(V2 = as.numeric(as.character(V2)),sign=ifelse(V2<0,"neg","pos"))
       
@@ -120,7 +120,7 @@ shinyServer(function(input, output) {
      ggplot(performance.data, aes(x=V1, y=V2)) + 
        geom_bar(stat="identity", aes(fill=sign)) + 
        guides(fill=FALSE) +
-       #coord_cartesian(ylim = c(-13, 13)) + 
+       coord_cartesian(ylim = c(-13, 13)) + 
        scale_fill_manual(values = c("neg"="red","pos"="green")) +
        geom_text(aes(label=sprintf("%.2f%%",round(V2,2)), vjust=1)) +
        xlab("Fund") + ylab("% Return") + 
